eae1fa2bd6a2666e2d01f5f1b61fe953b6238a4e
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-21 11:38:30 +0530

    Create solution_c6_saa1.py

---
+++ solution_c6_saa1.py
@@ -0,0 +1,13 @@
+# Creating a list 'numbers'
+numbers=[10,20,30,40,50]
+# Creating a variable 'count'
+# Initializing it to '0'
+count=0
+
+# Creating a 'for' loop to access every element in the list
+for num in numbers:
+  # Incrementing 'count' by '1'
+  count=count+1
+
+# Printing the value of 'count'
+print("Length:",count)
