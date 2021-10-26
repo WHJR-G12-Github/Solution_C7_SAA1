23e936327b212bca84dba1059bdbaad9c3b769af
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-26 14:29:08 +0530

    Create solution_c7_saa1.py

---
+++ solution_c7_saa1.py
@@ -0,0 +1,11 @@
+# Importing 'random' module
+import random
+
+# Creating a list 'dice'
+dice = [1,2,3,4,5,6]
+
+# Choosing a number randomly from the list 'dice'
+roll = random.choice(dice)
+
+# Printing the value
+print("Dice rolled",roll)
